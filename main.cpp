f69c94bffcba7c7b9848aa27d8fb2819c32a8827
3644 <baptistereiter2005@gmail.com>
2024-12-02 14:41:48 +0100

    Grid initialisée depuis fichier

--- main.cpp
+++ main.cpp
@@ -10,10 +10,9 @@ const int gridHeight = 80;
 std::vector<std::vector<int>> grid(gridWidth, std::vector<int>(gridHeight));
 
 void initializeGrid() {
-    std::srand(std::time(0));
     for (int x = 0; x < gridWidth; ++x) {
         for (int y = 0; y < gridHeight; ++y) {
-            grid[x][y] = std::rand() % 2;  // Randomly initialize cells as alive or dead
+            grid[x][y] = 0;  // Initialise toutes les cellules comme mortes 
         }
     }
 }
